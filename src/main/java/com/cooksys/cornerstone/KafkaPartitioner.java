f9ba457cd17b5dcc85636809ab2cc3b9bb5392f2
machray <machray505@hotmail.com>
2015-10-14 11:18:14 -0700

    added to pom

---
+++ src/main/java/com/cooksys/cornerstone/KafkaPartitioner.java
@@ -0,0 +1,19 @@
+package com.cooksys.cornerstone;
+
+import kafka.utils.VerifiableProperties;
+
+public class KafkaPartitioner {
+	public KafkaPartitioner (VerifiableProperties props) {
+		 
+    }
+ 
+    public int partition(Object key, int a_numPartitions) {
+        int partition = 0;
+        String stringKey = (String) key;
+        int offset = stringKey.lastIndexOf('.');
+        if (offset > 0) {
+           partition = Integer.parseInt( stringKey.substring(offset+1)) % a_numPartitions;
+        }
+       return partition;
+  }
+}
